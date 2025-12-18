acbe2f2917a1c6d9d924ed2cda31ba9d44818906
avinash <avinashraj0918@gmail.com>
2025-12-19 00:21:06 +0530

    updated_somefiles

--- vapi-node-server/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { interviewCovers } from "../constants";
-
-export const getRandomInterviewCover = () => {
-  const randomIndex = Math.floor(Math.random() * interviewCovers.length);
-  return `/covers${interviewCovers[randomIndex]}`;
-};
